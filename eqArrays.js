5c5b0038866db42389c1a994ad32bb9f7c0dc09e
Jamie Li <lijamie19828@gmail.com>
2021-10-26 22:37:18 +0000

    wrote function eqArray to return true or false depending on the two array inputs. eqArrays implements assertEqual to check if the arrays are Equal.

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    return `🛑Assertion Failed: ${actual} !== ${expected}`;
+  } else {
+    return `✅Assertion Passed: ${actual} === ${expected}`;
+  }
+};
+
+const eqArrays = function (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+//TEST CODE
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true))
+console.log(assertEqual(eqArrays([1, 2, 4], [1, 2, 3]), true))
+console.log(assertEqual(eqArrays([], []), true))
