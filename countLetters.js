7eca50aca1e4f9d33dac733fba7faa617668e4a4
Jamie Li <lijamie19828@gmail.com>
2021-10-28 00:14:10 +0000

    wrote countLetters to mutate a string to have no non letter characters in all lower cas. Then to run the string through a for loop and count how many times a character appears in a string

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return `Assertion Failed`;
+  } else {
+    return `Assertion Passed`;
+  }
+};
+//END ASSERTEQUAL
+
+const countLetters = function(str) {
+  const letterCount = {};
+  let strFormatted = str.toLowerCase().replace(/\W/g, '');
+  //console.log(strFormatted)
+  for (const char of strFormatted) {
+    // console.log(char)
+    letterCount[char] = letterCount[char] + 1 || 1;
+  }
+  return letterCount;
+};
+
+const test = "hello this is test a string";
+const result = countLetters(test);
+//TEST
+console.log(assertEqual(result["i"], 3));
+console.log(assertEqual(result["h"], 2));
+console.log(assertEqual(result["t"], 4));
+console.log(assertEqual(result["e"], 2));
