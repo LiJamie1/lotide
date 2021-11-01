3140a7a21b8feccc520aa4e47ed5778fb72892fa
Jamie Li <lijamie19828@gmail.com>
2021-11-01 04:30:03 +0000

    added recursion to eqObjects so it now works with nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -25,33 +25,47 @@ const eqObjects = function(o1, o2) {
     return false;
   }
   for (const key in o1) {
-    if (!(Array.isArray(o1[key]))) {
-      if (o1[key] !== o2[key]){
+    if ((Array.isArray(o1[key]))) {
+      if (!(eqArrays(o1[key], o2[key]))) {
         return false;
       };
     };
-    if (!(eqArrays(o1[key], o2[key]))) {
+    if (typeof o1[key] === 'object' && o1[key] !== null) {
+      if (!eqObjects(o1[key], o2[key])) {
+        return false;
+      }
+    } else if (o1[key] !== o2[key]){
       return false;
     };
   };
   return true;
 };
 
-// // TEST CODE
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-// console.log(eqObjects(ab, ba))
-const abc = {a: "1", b: "2", c: "3"}
-// console.log(eqObjects(ab, abc))
-// console.log(assertEqual(eqObjects(ab, ba), true))
-// console.log(assertEqual(eqObjects(ab, abc), false))
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-eqObjects(cd, dc); // => true
-console.log(assertEqual(eqObjects(cd, dc), true))
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-eqObjects(cd, cd2); // => false
-console.log(assertEqual(eqObjects(cd, cd2), true))
+//TEST CODE
+// Flat Objects
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// const abc = {a: "1", b: "2", c: "3"}
+// console.log(eqObjects(ab, ba)) // => true
+// console.log(eqObjects(ab, abc))// => false
+// console.log(assertEqual(eqObjects(ab, ba), true)) // => true
+// console.log(assertEqual(eqObjects(ab, abc), false)) // => true
 
+// Object with arrays
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// eqObjects(cd, dc); // => true
+// eqObjects(cd, cd2); // => false
+// console.log(assertEqual(eqObjects(cd, dc), true)) // => true
+// console.log(assertEqual(eqObjects(cd, cd2), true)) // => false
+
+// Nested Objects
+// console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => true
+// console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => false
+// console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })) // => false
+console.log(assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true)) // => true
+console.log(assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false)) // => false
+console.log(assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false)) // => false
