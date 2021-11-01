0c190e568f19328f9452b25dd12b97c31ad64765
Jamie Li <lijamie19828@gmail.com>
2021-11-01 07:16:50 +0000

    eqArraysTest updated for mocha and chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -2,9 +2,17 @@ const assert = require('chai').assert;
 const assertEqual = require('../assertEqual.js')
 const eqArrays = require('../eqArrays.js')
 
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));// => true
-console.log(assertEqual(eqArrays([1, 2, 4], [1, 2, 3]), true));// => false
-console.log(assertEqual(eqArrays([], []), true)); // => true
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)); // => true
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)); // => true
-console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true)); // => true
+describe('#eqArrays', function() {
+  it('should return assertion passed if inputs for eqArrays are equal', function() {
+    const input = [1,2,3];
+    const expected = [1,2,3];
+    const result = eqArrays(input, expected);
+    assert.strictEqual(assertEqual(result, true), 'Assertion Passed')
+  });
+  it('should return assertion passed if inputs for eqArrays are not equal', function() {
+    const input = [1,2,3];
+    const expected = [1,2,4];
+    const result = eqArrays(input, expected);
+    assert.strictEqual(assertEqual(result, true), 'Assertion Failed')
+  });
+});
